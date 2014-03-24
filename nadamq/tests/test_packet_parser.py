6b157500afd918831c909c49d7255d0b4769e893
Christian Fobel <christian@fobel.net>
2014-03-24 08:23:09 -0400

    Update test cases

--- nadamq/tests/test_packet_parser.py
+++ nadamq/tests/test_packet_parser.py
@@ -1,20 +1,16 @@
 from nose.tools import ok_, eq_
-from PacketParser import (cPacketParser, get_packet_data, PACKET_NAME_BY_TYPE,
-                          PACKET_TYPES)
+from nadamq.NadaMq import (cPacketParser, get_packet_data, PACKET_NAME_BY_TYPE,
+                           PACKET_TYPES)
 
 
-def parse_demo(command_byte, payload, crc_checksum=False):
-    packet_data = get_packet_data(command_byte, payload, crc_checksum)
+def parse_demo(interface_unique_id, packet_type, payload):
+    packet_data = get_packet_data(interface_unique_id, packet_type, payload)
     parser = cPacketParser()
     packet = parser.parse(packet_data)
     print 'packet_type: %s' % PACKET_NAME_BY_TYPE[packet.type_]
-    print 'command: %s' % hex(packet.command)
+    print 'command: %s' % hex(packet.iuid)
     print 'payload: "%s"' % packet.data()
-    if crc_checksum:
-        try:
-            print 'CRC matched: %s' % hex(packet.crc)
-        except RuntimeError:
-            pass
+    print 'CRC matched: %s' % hex(packet.crc)
     return packet
 
 
@@ -25,27 +21,27 @@ def main():
 
 
 def test_parse_request():
-    packet = parse_demo('0x80', 'hello')
+    packet = parse_demo(97, PACKET_TYPES.REQUEST, 'hello')
     eq_(packet.type_, PACKET_TYPES.REQUEST)
-    eq_(packet.command, 0)
+    eq_(packet.iuid, 97)
     eq_(packet.data(), 'hello')
 
-    packet = parse_demo('0xFF', 'hello', crc_checksum=True)
+    packet = parse_demo(10, PACKET_TYPES.REQUEST, 'world')
     eq_(packet.type_, PACKET_TYPES.REQUEST)
-    eq_(packet.command, 0x7F)
-    eq_(packet.data(), 'hello')
+    eq_(packet.iuid, 10)
+    eq_(packet.data(), 'world')
 
 
 def test_parse_response():
-    packet = parse_demo('0x00', 'hello')
+    packet = parse_demo(71, PACKET_TYPES.RESPONSE, 'foo')
     eq_(packet.type_, PACKET_TYPES.RESPONSE)
-    eq_(packet.command, 0x00)
-    eq_(packet.data(), 'hello')
+    eq_(packet.iuid, 71)
+    eq_(packet.data(), 'foo')
 
-    packet = parse_demo('0x79', 'blah')
+    packet = parse_demo(32, PACKET_TYPES.RESPONSE, 'bar')
     eq_(packet.type_, PACKET_TYPES.RESPONSE)
-    eq_(packet.command, 0x79)
-    eq_(packet.data(), 'blah')
+    eq_(packet.iuid, 32)
+    eq_(packet.data(), 'bar')
 
 
 def parse_args():
@@ -58,7 +54,6 @@ def parse_args():
     parser.add_argument('--crc', action='store_true', default=False)
     args = parser.parse_args()
     return args
-
 
 if __name__ == '__main__':
     main()
